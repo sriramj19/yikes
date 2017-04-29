599fbd067f4fe05ac157ad50c1b6dc3265888189
Sriram Jayarman <sriramemailsyou@gmail.com>
2017-04-29 08:27:20 +0530

    Update URL to localhost
    The routes are changed to local, due to failure with Heroku integration

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ angular.module('app')
     $scope.app = {
       name: 'Yikes!',
       version: '1.0.0',
-      apiURL: 'https://yikeschat.herokuapp.com/',
+      apiURL: 'http://localhost:1337/',
       landing: function() {
         if($localStorage.userDetails) {
           $state.go('dashboard')

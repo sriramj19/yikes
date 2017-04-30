80dfe1b72b6c0c8ccb0479ed071830557dc8f4d7
Sriram Jayaraman <sriramemailsyou@gmail.com>
2017-04-30 14:34:40 +0530

    [Bug Fix] Updating subscriptions among various devices

--- assets/js/controllers/ChatController.js
+++ assets/js/controllers/ChatController.js
@@ -3,7 +3,10 @@
 var app = angular.module('app',[]);
 /* Chats Controller */ /*Invloves all chat processing and chatroom activities*/
 
-app.controller('ChatController',['$http', '$scope', '$localStorage', 'ChatService', function($http, $scope, $localStorage, ChatService){
+app.controller('ChatController',
+  ['$http', '$scope', '$localStorage', 'ChatService',
+   function($http, $scope, $localStorage, ChatService){
+
   $scope.chatData =[];  //Consists of all Chat data for all the chat rooms of specified user
   $scope.currentChatRoom = {}; //The chat room currently using
   $scope.chats = []; //Chats persisting to the current chat room
@@ -52,41 +55,47 @@ app.controller('ChatController',['$http', '$scope', '$localStorage', 'ChatServic
   //Set the current chat room and its chat data
   $scope.setCurrentChatRoom = function(currentChatRoom) {
     var flag = false;
-    _.forEach($localStorage.userDetails.subscriptions, function(value) {
-      if(value === currentChatRoom.id) {
-        flag = true;
-      }
-    });
-    //View a subscribed chatroom
-    if(flag) {
-      $scope.currentChatRoom = _.clone(currentChatRoom);
-      $scope.chats=[];
-      _.forEach($scope.chatData, function(value) {
-        if(value.chatroom == $scope.currentChatRoom.id) {
-          $scope.chats.push(value);
+    ChatService.getUserSubscriptions($scope.app.apiURL, $localStorage.userDetails.id).then(function(response) {
+      _.forEach(response.subscriptions, function(value) {
+        if(value === currentChatRoom.id) {
+          flag = true;
         }
       });
-      if(!$scope.chats.length) {
-        alert('It seems lonely here, start a new conversation');
+      if($localStorage.userDetails.subscriptions !== response.subscriptions) {
+        $localStorage.userDetails.subscriptions = _.clone(response.subscriptions);
       }
-    }
-    //Else Subscribe and go into chatroom
-    else {
-      var confirmation = confirm('You are not subscribed to this chatroom... Do you wish to subscribe?');
-      if(confirmation) {
-        ChatService.subscribe($scope.app.apiURL, $localStorage.userDetails.id, currentChatRoom.id).then(function(response) {
-          if(response) {
-            $scope.currentChatRoom = _.clone(currentChatRoom);
-            $scope.chats=[];
-            _.forEach($scope.chatData, function(value) {
-              if(value.chatroom == $scope.currentChatRoom.id) {
-                $scope.chats.push(value);
-              }
-            });
+      //View a subscribed chatroom
+      if(flag) {
+        $scope.currentChatRoom = _.clone(currentChatRoom);
+        $scope.chats=[];
+        _.forEach($scope.chatData, function(value) {
+          if(value.chatroom == $scope.currentChatRoom.id) {
+            $scope.chats.push(value);
           }
         });
+        if(!$scope.chats.length) {
+          alert('It seems lonely here, start a new conversation');
+        }
       }
-    }
+      //Else Subscribe and go into chatroom
+      else {
+        var confirmation = confirm('You are not subscribed to this chatroom... Do you wish to subscribe?');
+        if(confirmation) {
+          ChatService.subscribe($scope.app.apiURL, $localStorage.userDetails.id, currentChatRoom.id).then(function(response) {
+            if(response) {
+              $localStorage.userDetails.subscriptions.push(currentChatRoom.id);
+              $scope.currentChatRoom = _.clone(currentChatRoom);
+              $scope.chats=[];
+              _.forEach($scope.chatData, function(value) {
+                if(value.chatroom == $scope.currentChatRoom.id) {
+                  $scope.chats.push(value);
+                }
+              });
+            }
+          });
+        }
+      }
+    });
   }
 
   //Get all chat records and start listening to the server for changes
